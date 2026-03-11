592c13e29bbc23336d13c12abd0f2f4901099598
Maleyka <13584@holbertonstudents.com>
2026-03-11 21:27:31 +0400

    Implement check_win method in Minesweeper
    Added a method to check for win condition in Minesweeper.

--- mines.py
+++ mines.py
@@ -51,6 +51,14 @@ class Minesweeper:
                         self.reveal(nx, ny)
         return True
 
+    def check_win(self):
+        """Return True if all non-mine cells are revealed"""
+        for y in range(self.height):
+            for x in range(self.width):
+                if not self.revealed[y][x] and (y * self.width + x) not in self.mines:
+                    return False
+        return True
+
     def play(self):
         while True:
             self.print_board()
@@ -60,6 +68,10 @@ class Minesweeper:
                 if not self.reveal(x, y):
                     self.print_board(reveal=True)
                     print("Game Over! You hit a mine.")
+                    break
+                if self.check_win():
+                    self.print_board(reveal=True)
+                    print("Congratulations! You've won the game.")
                     break
             except ValueError:
                 print("Invalid input. Please enter numbers only.")
